10a3b5af75f5d87d8ee28e8f0d4a0e879fb7c5f3
Josh Lalonde <mori.ryuuji3@gmail.com>
2023-05-06 14:02:47 -0300

    only show validity on blur

--- packages/form/src/hoc/withErrorMessage.tsx
+++ packages/form/src/hoc/withErrorMessage.tsx
@@ -1,22 +1,39 @@
+import { useState } from "react"
 import ErrorMessageContainer, { ErrorMessageContainerProps } from "../components/ErrorMessageContainer"
 
 function withErrorMessage<T extends object>(Element: React.ComponentType<T>) {
-    function withErrorMessageWrapper({ 
+    function Wrapper({ 
         isValid,
         errorMessage,
         className,
+        
+        onBlur,
+
         ...elementProps 
     }: ErrorMessageContainerProps & any) {
+        const [ hasBlurred, setHasBlurred ] = useState(false)
+
+        function handleBlur(e: React.FocusEvent<any>) {
+            onBlur?.(e)
+            setHasBlurred(true)
+        }
+
         return (
-            <ErrorMessageContainer isValid={isValid} errorMessage={errorMessage} className={className}>
-                <Element {...elementProps as T} />
+            <ErrorMessageContainer
+                {...(hasBlurred && {
+                    isValid,
+                    errorMessage,
+                })}
+                className={className}
+            >
+                <Element {...elementProps as T} onBlur={handleBlur} />
             </ErrorMessageContainer>
         )
     }
 
-    withErrorMessageWrapper.displayName = `withErrorMessage(${Element.displayName || Element.name})`
+    Wrapper.displayName = `withErrorMessage(${Element.displayName || Element.name})`
 
-    return withErrorMessageWrapper
+    return Wrapper
 }
 
 export default withErrorMessage
